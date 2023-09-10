10923e625f368b7f5df6cdc5be588c1f8ed51d82
sambhu619 <rsambhu619@gmail.com>
2023-09-10 17:23:11 -0400

    new file:   Docker/dockerinstall.sh 	new file:   Plex/plex-compose.yaml 	deleted:    dockerinstall.sh

---
+++ Docker/dockerinstall.sh
@@ -0,0 +1,9 @@
+sudo apt update && sudo apt upgrade -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+#install docker compose
+sudo apt-get install libffi-dev libssl-dev -y
+sudo apt install -y python3-dev
+sudo apt-get install -y python3 python3-pip
+sudo pip3 install docker-compose
+sudo systemctl enable docker
