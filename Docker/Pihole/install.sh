0674ce5f92aedf8fa7646ccea1eab2a9adaa3938
sambhu619 <rsambhu619@gmail.com>
2023-09-15 19:09:29 -0400

    new file:   Docker/Pihole/install.sh 	new file:   x.yaml

---
+++ Docker/Pihole/install.sh
@@ -0,0 +1,8 @@
+sudo mkdir ~/pihole
+sudo cp docker-compose.yaml ~/pihole
+cd ~/pihole
+sudo systemctl stop systemd-resolve
+sudo systemctl disable systemd-resolve
+sudo sed -i 's/nameserver 127.0.0.[0-9]*/nameserver 1.1.1.1/' /etc/resolv.conf
+sudo docker-compose up -d
+
