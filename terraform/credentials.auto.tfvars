29652da43616e94d8a9945d8bc1c2b3a7cf8310f
Sambhu Rajendran <rsambhu619@gmail.com>
2025-05-02 15:11:36 -0400

    Proxmox docker file updated for terraforn

---
+++ terraform/credentials.auto.tfvars
@@ -0,0 +1,3 @@
+proxmox_api_url      = "https://192.168.4.250:8006/api2/json"
+proxmox_api_token_id = "root@pam!terraform"
+proxmox_api_token    = "6078f540-3b99-4015-903b-8f31db62f6ff"
---
+++ terraform/provider_proxmox.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "3.0.1-rc8"
+    }
+  }
+}
+
+variable "proxmox_api_url" {
+  type = string
+}
+
+variable "proxmox_api_token_id" {
+  type = string
+}
+
+variable "proxmox_api_token" {
+  type = string
+}
+provider "proxmox" {
+  pm_api_url          = variable.proxmox_api_url
+  pm_api_token_id     = variable.proxmox_api_token_id
+  pm_api_token_secret = variable.proxmox_api_token
+  pm_tls_insecure     = false #to not verify checking for certificates
+
+}
