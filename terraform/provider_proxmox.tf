2769e72d8efec6f0bc158402f89504efb20aab21
Sambhu Rajendran <rsambhu619@gmail.com>
2025-05-02 16:20:27 -0400

    fixed errors in variables

--- terraform/provider_proxmox.tf
+++ terraform/provider_proxmox.tf
@@ -19,9 +19,9 @@ variable "proxmox_api_token" {
   type = string
 }
 provider "proxmox" {
-  pm_api_url          = variable.proxmox_api_url
-  pm_api_token_id     = variable.proxmox_api_token_id
-  pm_api_token_secret = variable.proxmox_api_token
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token
   pm_tls_insecure     = true #to not verify checking for certificates
 
 }
